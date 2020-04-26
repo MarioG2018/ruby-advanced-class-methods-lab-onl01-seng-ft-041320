570a53614399de77d304c2e1effeee7399dd8aae
Mario Garza <mario.garza02@utrgv.edu>
2020-04-26 16:17:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require pry
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,46 @@ class Song
 
   def save
     self.class.all << self
+  end
+  
+  
+  def self.create()
+    song=self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song=self.new
+    song.name=name
+    return song
+  end
+  def self.create_by_name(name)
+    song=self.new
+    song.name=name
+    song.save
+    return song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song|song.name==name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    return self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|letter|letter.name}
+  end
+  
+  def self.new_from_filename
+    song=self.new
+    binding.pry
+  end
+  def self.destroy_all
+    @@all.clear()
+  
   end
 
 end
